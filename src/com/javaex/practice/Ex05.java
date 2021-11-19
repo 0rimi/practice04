8487089c5f50c2dbce55874830bb5ad95ed40bf2
이영림 <rm1113@naver.com>
2021-11-19 14:55:15 +0900

    Ex05

---
+++ src/com/javaex/practice/Ex05.java
@@ -0,0 +1,24 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double sum=0;
+		for(int i=0; i<5; i++) {
+			double num = sc.nextDouble();
+			sum=sum+num;
+		}
+		
+		System.out.println("평균은 "+sum/5.0+" 입니다.");
+		
+		
+		sc.close();
+		
+	}
+
+}

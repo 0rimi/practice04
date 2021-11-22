a0d9cb8e9eba605c66b463f90f662c207e7f66bd
이영림 <rm1113@naver.com>
2021-11-22 13:04:09 +0900

    정답보고 예쁘게 고침

--- src/com/javaex/practice/Ex05.java
+++ src/com/javaex/practice/Ex05.java
@@ -8,13 +8,32 @@ public class Ex05 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		double sum=0;
-		for(int i=0; i<5; i++) {
-			double num = sc.nextDouble();
-			sum=sum+num;
-		}
 		
-		System.out.println("평균은 "+sum/5.0+" 입니다.");
+		int[] accdata = new int[5];
+		int num;
+		double sum = 0;
+
+		for (int i = 0; i < accdata.length; i++) {
+			num = sc.nextInt();
+			accdata[i] = num;
+		}
+
+		for (int i = 0; i < accdata.length; i++) {
+			sum = sum + accdata[i];
+		}
+
+		System.out.println("평균은 " + sum / accdata.length + " 입니다.");
+		
+		
+		
+		
+//		double sum=0;
+//		for(int i=0; i<5; i++) {
+//			double num = sc.nextDouble();
+//			sum=sum+num;
+//		}
+//		
+//		System.out.println("평균은 "+sum/5.0+" 입니다.");
 		
 		
 		sc.close();
